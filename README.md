60549ea7100c01f2f0b7499ea82b6e88dafa410d
Jeff Robbins <jeff@jjeff.com>
2023-10-27 12:03:59 -0400

    WIP

--- README.md
+++ README.md
@@ -1,15 +1,74 @@
-# jjeff-typescript-template
+# C3P.OTP
+
+```
+                                     /~\                           
+                                    |o o)      We're doomed!       
+                                    _\=/_                          
+                    ___        #   /  _  \   #                     
+                   /() \        \\//|/.\|\\//                      
+                 _|_____|_       \/  \_/  \/                       
+                | | === | |         |\ /|                          
+                |_|  O  |_|         \_ _/                          
+                 ||  O  ||          | | |                          
+                 ||__*__||          | | |                          
+                |~ \___/ ~|         []|[]                          
+                /=\ /=\ /=\         | | |                          
+________________[_]_[_]_[_]________/_]_[_\_________________________
+```
 
 ## Description
 
-This is a template for a typescript project. It includes the following:
+This is a simple one-time-password handler (with a Star Wars theme). The class creates a set of form fields that act as one input field. The user can enter the OTP in the first field and the focus will automatically move to the next field. The user can also use the backspace key to move to the previous field. Pasting into the first field will automatically fill the rest of the fields. Input will be automatically submitted when the last field is filled. A callback function can be used to handle the input. Alternately, the class will emit custom events when any input has been entered as well as when all fields are full. There is also an option to add a submit button.
 
-- [x] Typescript
-- [x] Mocha
-- [x] Chai
-- [x] ESLint
-- [x] Prettier
-- [x] Semantic Release
-- [x] JSDoc
-- [x] Github Actions
-- [x] VS Code Formatting, Linting, and Validation Settings
+## Usage
+
+### HTML
+
+```html
+<div id="otp"></div>
+```
+
+### JavaScript
+
+```javascript
+const otp = new OtpSet({
+    container: document.getElementById('otp'),
+    fields: 6,
+    callback: function (otp) {
+        console.log(otp);
+    }
+});
+```
+
+## Options
+
+| Option | Type | Default | Description |
+| --- | --- | --- | --- |
+| container | HTMLElement | null | The container element for the OTP fields. |
+| fields | Number | 6 | The number of OTP fields to create. |
+| callback | Function | null | An optional callback function to handle the OTP. |
+| submit | Boolean | false | Whether or not to add a submit button. |
+| submitText | String | 'Submit' | The text to display on the submit button. |
+| submitClass | String | 'otp-submit' | The class to add to the submit button. |
+
+## Events
+
+| Event | Description |
+| --- | --- |
+| otp-input | Fired when any input has been entered. |
+| otp-complete | Fired when all fields are full. |
+
+## Methods
+
+| Method | Description |
+| --- | --- |
+| reset() | Resets the OTP fields. |
+| destroy() | Destroys the OTP fields. |
+
+## License
+
+MIT License
+
+## Author
+
+Jeff Robbins
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,159 @@
-/**
- * A test function that returns a string
- *
- * @returns {string} Hello World!
- */
-export function testMe(): string {
-  return 'Hello World!'
+// Implement OTP set from README.md
+
+export type OtpSetOptions = {
+  container: HTMLElement,
+  fields: number,
+  callback?: (otp: string) => void,
+  submit?: boolean,
+  submitText?: string,
+  submitClass?: string,
+  css?: string,
+}
+export class OtpSet {
+  private container: HTMLElement;
+  private fields: number;
+  private callback: (otp: string) => void;
+  private submit: boolean;
+  private submitText: string;
+  private submitClass: string;
+  private css: string;
+
+  constructor(options: OtpSetOptions) {
+    this.container = options.container;
+    this.fields = options.fields;
+    this.callback = options.callback || (() => {});
+    this.submit = options.submit || false;
+    this.submitText = options.submitText || 'Submit';
+    this.submitClass = options.submitClass || 'otp-submit';
+    this.container.classList.add('otp-container');
+    this.css = options.css || '';
+    
+    this.init();
+  }
+
+  /**
+   * Initialize OTP set
+   */
+  private init() {
+    this.container.innerHTML = '';
+    const inputsDiv = document.createElement('div');
+    inputsDiv.classList.add('otp-inputs');
+    this.container.appendChild(inputsDiv);
+    for (let i = 0; i < this.fields; i++) {
+      const input = document.createElement('input');
+      input.type = 'text';
+      input.maxLength = 1;
+      input.addEventListener('keyup', (e) => {
+        const target = e.target as HTMLInputElement;
+        if (target.value.length === 1) {
+          const next = target.nextElementSibling as HTMLInputElement;
+          if (next) {
+            next.focus();
+          } else {
+            target.blur();
+            if (this.submit) {
+              this.submitForm();
+            }
+          }
+        }
+      });
+      inputsDiv.appendChild(input);
+    }
+    if (this.submit) {
+      const button = document.createElement('button');
+      button.type = 'button';
+      button.innerText = this.submitText;
+      button.classList.add(this.submitClass);
+      button.addEventListener('click', () => {
+        this.submitForm();
+      });
+      this.container.appendChild(button);
+    }
+
+    const style = document.createElement('style');
+    style.innerHTML = `
+      .otp-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+      .otp-inputs {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      .otp-inputs input {
+        width: 40px;
+        height: 40px;
+        margin: 0 5px;
+        text-align: center;
+        font-size: 20px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+      }
+      .otp-inputs input:focus {
+        outline: none;
+        border-color: #000;
+      }
+      .otp-submit {
+        margin-top: 10px;
+        padding: 5px 10px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        background: #fff;
+        cursor: pointer;
+      }
+      ${this.css}
+    `;
+    this.container.appendChild(style);
+  }
+
+  /**
+   * Submit form
+   */
+  private submitForm() {
+    const otp = this.getOtp();
+    this.callback(otp);
+    this.emit('otp-complete', otp);
+  }
+
+  /**
+   * Get OTP string from input fields 
+   * @returns OTP string
+   */
+  private getOtp(): string {
+    const inputs = this.container.querySelectorAll('input');
+    let otp = '';
+    inputs.forEach((input) => {
+      otp += input.value;
+    });
+    return otp;
+  }
+
+  listeners = new Set<(otp: string) => void>();
+
+  on(event: 'otp-input' | 'otp-complete', callback: (otp: string) => void) {
+    this.listeners.add(callback);
+  }
+
+  off(event: 'otp-input' | 'otp-complete', callback: (otp: string) => void) {
+    this.listeners.delete(callback);
+  }
+
+  emit(event: 'otp-input' | 'otp-complete', otp: string) {
+    this.listeners.forEach((listener) => {
+      listener(otp);
+    });
+  }
+
+  /**
+   * Reset OTP set
+   */
+  reset() {
+    this.init();
+  }
+
+  destroy() {
+    this.container.innerHTML = '';
+  }
 }
